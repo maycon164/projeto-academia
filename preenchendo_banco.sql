4595973693ce5254ed67a3a75bb9fc6e4722b7ca
maycon164 <mayconfelipe164@gmail.com>
2021-11-10 20:16:22 -0300

    inserindo dados no banco

---
+++ preenchendo_banco.sql
@@ -0,0 +1,162 @@
+USE academiadb;
+
+---------------------------------INSERINDO PESSOAS
+INSERT INTO pessoa (cpf, nome, nascimento, email, telefone, rua, bairro, num) VALUES
+--ALUNOS
+('50409978841', 'Felipe', '2002-11-21', 'teste@gmail.com','16899-3423', 'Rua das Laranjas', 'Laranjeiras', '191'),
+('82259101097', 'José', '1991-11-21', 'jose@gmail.com', '73372-7435','Avenida XV', 'Bairro A', '19'),
+('41264118015', 'Maria', '1992-11-11', 'maria@gmail.com','10697-2851' , 'Rua Anecy Rocha', 'Bairro A', '159'),
+('98757864039', 'Rafael', '1990-02-15', 'rafaael@gmail.com', '12378-8371' , 'Avenida Vitoria', 'Laranjeiras', '189'),
+('95387925052', 'Yasmim', '2001-12-14', 'yasmim@gmail.com', '15702-4917','Rua Anecy Rocha ', 'Bairro A', '89'),
+
+--INSTRUTORES
+('39311504025', 'Samira', '1999-11-21', 'sami@gmail.com', '73837-3687','Rua dos Limoes', 'Limoeiro', '129'),
+('96319340045', 'Karen', '1989-03-21', 'karen@gmail.com', '43355-5988', 'Rua dos Limoes', 'Limoeiro', '171'),
+('05460595037', 'Renan', '2002-11-21', 'renan@gmail.com', '28260-8490','Rua das Laranjas', 'Laranjeiras', '28')
+
+
+---INSTRUTORES
+INSERT INTO instrutor (cpf, ativo, especializacao) VALUES
+('39311504025', 1, 'Dança e Consciência Corporal'),
+('96319340045', 1, 'Condicionamento Físico no Envelhecimento'),
+('05460595037', 1, 'Educação Especial')
+
+
+
+
+---ALUNOS
+INSERT INTO aluno(cpf, ativo) VALUES 
+('50409978841', 1),
+('82259101097', 1),
+('41264118015', 1),
+('98757864039', 0),
+('95387925052', 1)
+
+
+
+---------------------------------INSERINDO PLANOS
+
+INSERT
+	INTO
+	plano(nome,
+	descricao,
+	preco,
+	duracao)
+VALUES
+
+('Musculação',
+'O treinamento de força é um exercício, ou um conjunto de exercícios, 
+que ajuda os diferentes músculos do seu corpo para se tornar mais forte',
+60.99,
+60),
+
+('Boxe','Boxe ou pugilismo é um esporte de combate no qual os lutadores 
+calçam luvas acolchoadas e utilizam as mãos para atacar e defender', 99.99, 45),
+
+('Natação', 'um dos exercícios mais completos por movimentar grande parte 
+dos músculos e articulações do corpo', 100.29, 60),
+
+('Ginástica', ' a prática de uma série de movimentos exigentes de força, flexibilidade 
+e coordenação motora para fins únicos de aperfeiçoamento físico e mental', 45.99, 30)
+
+--------------------------------- RELACIONANDO PLANOS COM INSTRUTORES
+INSERT INTO instrutor_plano(cpf_instrutor, id_plano) VALUES
+('05460595037', 1),
+('05460595037', 2),
+('39311504025', 3),
+('39311504025', 1),
+('96319340045', 4)
+
+--------------------------------- RELACIONANDO PLANOS COM ALUNOS
+INSERT INTO aluno_plano(cpf_aluno, id_plano, data_inicio, data_expiracao) VALUES
+('50409978841', 1, default, DATEADD(DAY, 60, GETDATE())),
+('82259101097', 1, default, DATEADD(DAY, 60, GETDATE())),
+('95387925052', 3, default, DATEADD(DAY, 60, GETDATE())),
+('98757864039', 2, default, DATEADD(DAY, 45, GETDATE())),
+('41264118015', 4, default, DATEADD(DAY, 30, GETDATE()))
+
+
+--------------------------------- RELACIONANDO PLANO_ALUNO COM PAGAMENTOS
+/*
+ * 1 maria, ginastica 45.99
+ * 2 felipe, musculacao 60.99
+ * 3 jose, musculacao,
+ * 4 yasmim, natacao 100.29
+ * 5 rafael, boxe 99.99
+ * */
+INSERT INTO pagamento (id_aluno_plano, data_pagamento, valor_total, forma_pagamento, situacao) VALUES
+(1, null, 45.99, null, 'NÃO PAGO' ),
+(2, null, 60.99, null, 'NÃO PAGO' ),
+(3, GETDATE(), 60.99, 'DINHEIRO', 'PAGO' ),
+(4, GETDATE(), 100.29, 'CARTÃO', 'PAGO' ),
+(5, null, 99.99, null, 'NÃO PAGO' )
+
+SELECT * FROM pagamento;
+DELETE pagamento;
+
+--------------------------------- POR ENQUANTO O SISTEMA NÃO TEM AVALIAÇÕES FEITASA
+
+
+SELECT * FROM aluno_plano;
+
+UPDATE pessoa
+SET email = 'felipe@gmail.com'
+WHERE nome = 'Felipe'
+
+----QUERY QUE RETORNA TODOS OS INSTRUTORES
+SELECT p.*, i.ativo, i.especializacao
+FROM pessoa p, instrutor i 
+WHERE p.cpf = i.cpf 
+
+---QUERY RETORNA TODOS OS ALUNOS
+SELECT p.*, a.ativo, a.observacoes
+FROM pessoa p, aluno a
+WHERE p.cpf = a.cpf 
+
+-----QUERY QUE RETORNA OS INTRUTORES E OS PLANOS QUE ELES AUXILIAM
+SELECT i.cpf, pe.nome, p.nome AS plano
+FROM instrutor i, plano p, instrutor_plano ip, pessoa pe
+WHERE pe.cpf = i.cpf 
+AND ip.cpf_instrutor = i.cpf 
+AND ip.id_plano = p.id_plano 
+
+---QUERY QUE RETORNA OS NOME DO ALUNO, O PLANO QUE ASSINOU, DATA_INICIO, DATA_EXPIRACAO DO PLANO
+SELECT ap.id_aluno_plano AS id, a.cpf, p.nome, pl.nome AS plano, pl.preco, pl.duracao AS dias,
+CONVERT(CHAR(11), ap.data_inicio, 103) AS inicio, 
+CONVERT(CHAR(11),ap.data_expiracao, 103 ) as expira
+FROM pessoa p, aluno a, plano pl, aluno_plano ap 
+WHERE p.cpf = a.cpf
+AND pl.id_plano = ap.id_plano 
+AND a.cpf = ap.cpf_aluno
+
+---QUERY QUE RETORNA nome_aluno, nome_plano, data_pagamento, valor_total
+SELECT p.nome, a.cpf, pl.nome, pag.data_pagamento, pag.valor_total
+FROM pessoa p, aluno a, plano pl, pagamento pag, aluno_plano ap
+WHERE p.cpf = a.cpf
+AND ap.cpf_aluno = a.cpf
+AND ap.id_plano = pl.id_plano
+AND pag.id_aluno_plano = ap.id_aluno_plano 
+
+---QUERY QUE RETORNA nome_aluno, plano, data_pagamento, valor_total, nome_instrutor, data_inicio, data_expiracao
+----- NÃO CONSIGUE FAZER ESSA, TENTAR DEPOIS 
+SELECT p.nome, a.cpf, pl.nome, pag.data_pagamento, pag.valor_total, p.nome as instrutor
+FROM pessoa p, aluno a, plano pl, pagamento pag, aluno_plano ap, instrutor i, instrutor_plano ip 
+WHERE p.cpf = a.cpf
+AND ap.cpf_aluno = a.cpf
+AND ap.id_plano = pl.id_plano
+AND ip.cpf_instrutor = i.cpf 
+AND pag.id_aluno_plano = ap.id_aluno_plano 
+
+
+SELECT * FROM avaliacao;
+SELECT * FROM pagamento;
+
+exec sp_columns pessoa;
+exec sp_columns instrutor;
+exec sp_columns aluno;
+exec sp_columns plano;
+exec sp_columns instrutor_plano;
+exec sp_columns aluno_plano;
+exec sp_columns pagamento;
+
+
